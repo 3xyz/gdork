ea5460b192e6a202149b44dd75588efecb159d2a
tinker <nurislam.khalimov1014@gmail.com>
2023-07-15 13:18:54 +0300

    fix

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 ln -s "$(pwd)/gdorks.sh" ~/.local/bin/gdorks
-echo "Created sym link to ~/.local/bin/gdorks"
-echo "Now you can use tool from everywhere by typing gdorks -d ..."
-echo "Keep in mind that if you delete this folder or move, you will need to again run setup.sh"
+if [[ $? == 1 ]]; then
+  echo 'Something went wring'
+else 
+  echo "Created sym link to ~/.local/bin/gdorks"
+  echo "Now you can use tool from everywhere by typing gdorks -d ..."
+  echo "Keep in mind that if you delete this folder or move, you will need to again run setup.sh"
+fi
